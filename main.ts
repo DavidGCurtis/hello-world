dcf84169529ca2e1bc83ce73c226d9377475d4dc
DavidGCurtis <97483994+DavidGCurtis@users.noreply.github.com>
2022-01-10 17:08:54 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+})
